679151839a45055a81965cfa34426fbedd5666be
LC-space <67886919+LC-space@users.noreply.github.com>
2021-06-13 16:49:54 +0800

    Add files via upload

---
+++ Date and time/julian_day.py
@@ -0,0 +1,9 @@
+# julian_day.py
+#!/usr/bin/python
+
+from PyQt6.QtCore import QDate, Qt
+
+now = QDate.currentDate()
+
+print('Gregorian date for today:', now.toString(Qt.DateFormat.ISODate))
+print('Julian day for today:', now.toJulianDay()) 
