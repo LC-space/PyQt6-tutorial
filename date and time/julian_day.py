451bcaff5cbc8a5358a78bc29c12a0a2950acabd
LC-space <67886919+LC-space@users.noreply.github.com>
2021-06-13 16:49:32 +0800

    Delete date and time directory

--- date and time/julian_day.py
+++
@@ -1,9 +0,0 @@
-# julian_day.py
-#!/usr/bin/python
-
-from PyQt6.QtCore import QDate, Qt
-
-now = QDate.currentDate()
-
-print('Gregorian date for today:', now.toString(Qt.DateFormat.ISODate))
-print('Julian day for today:', now.toJulianDay()) 
